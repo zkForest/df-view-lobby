e1566d9c146c8424cb8b4ed04abedf5e4e6f42e5
MJ659600 <73265012+MJ659600@users.noreply.github.com>
2022-03-26 15:45:11 +0800

    testing moving utils over

--- public/utils-zkf.js
+++ public/utils-zkf.js
@@ -1,1 +1,8 @@
-let ABCDEF="test this";
+export function hasUncomfirmedUpgradeTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedUpgradeTx)}
+export function hasUncomfirmedProspectPlanetTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedProspectPlanetTx)}
+export function hasUncomfirmedFindArtifactTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedFindArtifactTx)}
+export function hasUncomfirmedActivateArtifactTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedActivateArtifactTx)}
+export function hasUncomfirmedDeactivateArtifactTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedDeactivateArtifactTx)}
+export function hasUncomfirmedWithdrawArtifactTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedWithdrawArtifactTx)}
+export function hasUncomfirmedInvadePlanetTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedInvadeTx)}
+export function hasUncomfirmedCapturePlanetTx(p) {return p.transactions?.hasTransaction(window.serde.isUnconfirmedCapturePlanetTx)}
